90799c2c38f5c0681cfbe52b9323f57b5221034d
La <35015234+Las-Wonho@users.noreply.github.com>
2018-03-21 19:41:20 +0900

    Express pramework added

--- Server/Server.js
+++ Server/Server.js
@@ -1,3 +1,14 @@
 var l = require("lambda-js");
-var list = [1,2,3,4,5,6,74];
-list.forEach(x->{consle.log(x)});
+var Server_Start_Message=["Las a.k.a Desire","Server Start"];
+var express = require('express');
+var app = express();
+var print = console.log;
+
+var server = app.listen(3000, function(){
+    Server_Start_Message.forEach(x=>console.log(x));
+})
+
+app.get('/', function(req, res){
+    print(res.type);
+    res.send("FUCK",);
+});
