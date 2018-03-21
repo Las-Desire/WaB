61be37ec4005bb4324e6fe45f01703edd6636217
La <35015234+Las-Wonho@users.noreply.github.com>
2018-03-21 20:52:38 +0900

    add router

--- Server/Server.js
+++ Server/Server.js
@@ -1,14 +1,25 @@
-var l = require("lambda-js");
-var Server_Start_Message=["Las a.k.a Desire","Server Start"];
+var debug = require('debug');
 var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var LAS = require('./routes/LAS');
+var l = require("lambda-js");
+
+
+var Server_Start_Message=["Las a.k.a Desire","Server Start"];
 var app = express();
 var print = console.log;
 
 
-var server = app.listen(3000, function(){
+var server = app.listen(3000, ()=>{
     Server_Start_Message.forEach(x=>console.log(x));
 })
 
 app.get('/', (req, res)=>{
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+    print(req.url);
     res.send("<h1>FUCK</h1>");
 });
---
+++ Server/routes/LAS.js
@@ -0,0 +1,40 @@
+﻿'use strict';
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', (req, res) => {
+    res.send('LAS');
+
+
+});
+
+router.get('/keyboard', function (req, res) {
+
+    var message = {
+        type: 'buttons',
+        buttons: [
+            'LAS TEST1',
+            'LAS TEST2'
+        ]
+    };
+    res.set({
+        'content-type': 'application/json'
+    }).send(JSON.stringify(message));
+});
+
+router.post('/message', function (req, res) {
+
+    var message = {
+        type: 'text',
+        text: "LAS 01"
+    };
+    res.set({
+        'content-type': 'application/json'
+    }).send(JSON.stringify(message));
+});
+
+
+
+
+module.exports = router;
