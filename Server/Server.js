241c2efe62b503fd11f9bc83c766167937ec6fd1
La <35015234+Las-Wonho@users.noreply.github.com>
2018-04-09 18:55:42 +0900

    set...

--- Server/Server.js
+++ Server/Server.js
@@ -1,21 +1,29 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var fs = require("fs");
 
 var LAS = require('./routes/LAS');
-var l = require("lambda-js");
 
 var Server_Start_Message=["Las a.k.a Desire","Server Start"];
+
+var send_html = (res, path)=>{
+    fs.readFile(path,  function(err, data) {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end(data);
+    });
+};
+
+
+io.sockets.on('connection', function (socket) {
+    console.log('ll');
+});
+
+app.get('/las',(res,req)=>{
+    console.log('las');
+    send_html(req,"./HTML/Main.html");
+});
 
 http.listen(3000, function () {
     Server_Start_Message.forEach(x=>console.log(x));
 });
-
-app.use('/', LAS);
-
-app.use(function (req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
