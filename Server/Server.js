accd9615280a9df3c4f8b33de26787541a77dbc2
La <35015234+Las-Wonho@users.noreply.github.com>
2018-04-19 17:06:18 +0900

    지금 됨. 클라이언트에서 서버로 보내기 성공
    이제 남은건 제일 중요한 캔버스가 남음

--- Server/Server.js
+++ Server/Server.js
@@ -1,4 +1,9 @@
 
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 var Server_Start_Message=["Las a.k.a Desire","Server Start"];
 var app = require('express')();
 var http = require('http').Server(app);
@@ -13,16 +18,16 @@ var i=0;
 io.on('connection', function (socket) {
     console.log(i);
     i=i+1;
-    socket.emit('a',i);
-    io.to(socket.id).emit('qw',2);
-
-    socket.on('qwe',data=>{
-        console.log('data   ');
+    socket.emit('connection',i);
+    socket.on('d',(x)=>{
+        console.log(x);
     });
 });
+rl.on('line', (line) => {
+    io.emit('direct-message',line);
+});
 
-
-http.listen(3001, function () {
-    console.log('listening on *:3000');
+http.listen(3000, function () {
+    console.log('listening on *:3000'); 
 });
 
--- Server/Todo_List.md
+++ Server/Todo_List.md
@@ -1,2 +1,2 @@
 <h1>Todo List</h1>
-<h2>Set a form and sumit used buttons like php to node.js Server</h2>
+<h2>canvus setting, send client to server</h2>
